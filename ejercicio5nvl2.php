3a5f355fc4079affdd93f8ef257750ca66b60745
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-02-19 21:48:37 +0100

    lvl2ejercicio1

---
+++ ejercicio5nvl2.php
@@ -0,0 +1,7 @@
+<?php
+/*Exercici 1
+Crea un programa que contingui dos arrays de nombres enters/floats. Un cop creats, mostra per pantalla el resultat de:
+
+La intersecció dels dos arrays (nombres en comú)
+La mescla dels dos arrays.*/
+?>
