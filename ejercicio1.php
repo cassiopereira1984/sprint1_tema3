b7d4454f05255be5cc5df4af3fb56791ba7210dd
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-02-19 11:03:55 +0100

    ejercicio1

---
+++ ejercicio1.php
@@ -0,0 +1,7 @@
+<?php
+/*Exercici 1
+Crea un array, afegeix-li 5 nombres enters i després mostrals per pantalla d’un en un.*/
+$n = array(2, 4, 5, 8, 14);
+echo $n[0] . $n[1] . $n[2] . $n[3] . $n[4] . $n[5];
+
+?>
