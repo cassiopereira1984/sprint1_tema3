26cdff7bdc5d9f735dc687cbd628471b335cfe61
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-02-19 13:27:35 +0100

    ejercicio4 + finish

---
+++ ejercicio4.php
@@ -0,0 +1,16 @@
+<?php
+/*- Exercici 4
+Fes un array associatiu que representi informació de tu mateix/a. En concret ha d’incloure:
+
+nom
+edat
+email
+menjar*/
+$array = array("nombre" => "Cassio", "edat" => "40", "email" => "cas@cas.com", "menjar" => "bbq");
+
+echo $array["nombre"] . "\n";
+echo $array["edat"] . "\n";
+echo $array["email"] . "\n";
+echo $array["menjar"];
+
+?>
