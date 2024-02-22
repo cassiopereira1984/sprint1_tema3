0a3c06d05ed4a9594c5d3206f7687205512f8eda
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-02-22 13:09:08 +0100

    ejercicio1 + foreach + print_r

--- ejercicio1.php
+++ ejercicio1.php
@@ -3,5 +3,8 @@
 Crea un array, afegeix-li 5 nombres enters i després mostrals per pantalla d’un en un.*/
 $n = array(2, 4, 5, 8, 14);
 echo $n[0] . $n[1] . $n[2] . $n[3] . $n[4];
-
+foreach($n as $array){
+    echo $array;    
+}
+print_r($n);
 ?>
